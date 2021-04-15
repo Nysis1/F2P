6be12dc8f117543cdaeb8db71065fb8314d83757
NicolasPandraud <nicolas.pandraud@riskee.fr>
2021-04-15 16:11:56 +0200

    add CORS on game service

--- src/app/common/services/game.service.ts
+++ src/app/common/services/game.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { Game } from '../interfaces/game'
 import { environment } from '../../../environments/environment'
 import { map } from 'rxjs/operators'
+
+const optionRequete = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*',
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +19,7 @@ export class GameService {
   constructor(public http: HttpClient) {}
 
   list(): Observable<Game[]> {
-    return this.http.get(environment.apiBaseUrl + 'games').pipe(
+    return this.http.get(environment.apiBaseUrl + 'games', optionRequete).pipe(
       map((res: Object) => <Game[]>res),
     )
   }
